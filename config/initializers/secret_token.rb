6a976c2de4664bd5104f9ec2ce8ec0b4e68a2766
John Fletcher <net@twoedged.org>
2010-12-08 10:39:31 +0100

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Registrations::Application.config.secret_token = 'a0fe36e0927517da7df32379206932f26621c57429ee0168ffa1f60a3dc235177e0e1da7581ed05513487df7a3320797dc7189f2f6032de24f17095840236305'
