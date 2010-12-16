70700d07fca97581cb9850142d8bfb9e1be47c71
John Fletcher <net@twoedged.org>
2010-12-16 22:39:28 +0100

    Fixed ages hash bug again

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -8,12 +8,15 @@ class Registration < ActiveRecord::Base
 	SEX_FEMALE = 'Female'
 	SEXES = [SEX_MALE, SEX_FEMALE]
 
-	AGES = [ ['Adult', 18], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7],
-		[8, 8], [9, 9], [10, 10], [11, 11], [12, 12] ]
-	
+#	AGES = [ ['Adult', 18], [12, 12], [11, 11], [10, 10], [9, 9], [8, 8], [7, 7], 
+#		[6, 6], [5, 5], [4, 4], [3, 3], [2, 2], [1, 1], [0, 0] ]
+	AGES = { 'Adult' => 18, 12 => 12, 11 => 11, 10 => 10, 9 => 9, 8 => 8,
+		7 => 7, 6 => 6, 5 => 5, 4 => 4, 3 => 3, 2 => 2, 1 => 1, 0 => 0 }
+
 	validates :name, :presence => true,
 		:length => { :minimum => 4, :maximum => 20 }
 	validates :sex, :presence => true,
 		:inclusion => { :in => SEXES }
-	validates :age, :presence => true
+	validates :age, :presence => true,
+		:inclusion => { :in => AGES.values }
 end
