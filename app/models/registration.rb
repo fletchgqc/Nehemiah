3a07cfe264b88259c5645bc646e74d42608654df
John Fletcher <net@twoedged.org>
2010-12-16 03:02:17 +0100

    Bugfix for bad age ordering

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -8,13 +8,12 @@ class Registration < ActiveRecord::Base
 	SEX_FEMALE = 'Female'
 	SEXES = [SEX_MALE, SEX_FEMALE]
 
-	AGES = {'Adult' => 18, 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7,
-		8 => 8, 9 => 9, 10 => 10, 11 => 11, 12 => 12}
+	AGES = [ ['Adult', 18], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7],
+		[8, 8], [9, 9], [10, 10], [11, 11], [12, 12] ]
 	
 	validates :name, :presence => true,
 		:length => { :minimum => 4, :maximum => 20 }
 	validates :sex, :presence => true,
 		:inclusion => { :in => SEXES }
-	validates :age, :presence => true,
-		:inclusion => { :in => AGES.values }
+	validates :age, :presence => true
 end
